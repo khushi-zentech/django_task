85db5e7fcb1b3fcf53c5d7c0cff79b5e91260902
khushi-zentech <khushi.vaghela@zentechsolution.com>
2026-04-02 15:31:34 +0530

    [implementation of relationships and hyperlinked APIs]

--- drftutorial_1/snippets/views.py
+++ drftutorial_1/snippets/views.py
@@ -1,11 +1,27 @@
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework import renderers
+from rest_framework.response import Response
+
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
+
 from django.contrib.auth.models import User
+
 from snippets.models import Snippet
 from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
 
 # Create your views here.
+@api_view(["GET"])
+def api_root(request, format=None):
+    return Response(
+        {
+            "users": reverse("user-list", request=request, format=format),
+            "snippets": reverse("snippet-list", request=request, format=format),
+        }
+    )
+    
 class SnippetList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     
@@ -28,3 +44,11 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
